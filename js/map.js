0eda905e21126699fee5dbac68caa820daebeeed
Redrica <de.spring@gmail.com>
2018-04-10 04:02:43 +0300

    Сделала генерацию массива объявлений

--- js/map.js
+++ js/map.js
@@ -1,65 +1,110 @@
 'use strict';
 
-var OBJECT_COUNT = 8;
+var MAX_PICTURE_NUMBER = 8;
 var TITLES = ['"Большая уютная квартира"', '"Маленькая неуютная квартира"', '"Огромный прекрасный дворец"', '"Маленький ужасный дворец"', '"Красивый гостевой домик"', '"Некрасивый негостеприимный домик"', '"Уютное бунгало далеко от моря"', '"Неуютное бунгало по колено в воде"'];
-// Значения не должны повторяться.
 var MIN_PRICE = 1000;
 var MAX_PRICE = 1000000;
-var TYPE = ['palace', 'flat', 'house', 'bungalo'];
+var TYPES = ['palace', 'flat', 'house', 'bungalo'];
 var ROOMS = [1, 2, 3, 4, 5];
-var CHECK_TIME = ['12:00', '13:00', '14:00'];
-var FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-var PHOTOS = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
+var CHECK_TIMES = ['12:00', '13:00', '14:00'];
+var FEATURES_OPTIONS = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var MIN_ARRAY_LENGTH = 1;
+var PHOTOS_COLLECTION = ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg'];
 var MIN_X = 300;
 var MAX_X = 900;
 var MIN_Y = 150;
 var MAX_Y = 500;
+var MIN_GUESTS = 1;
+var MAX_GUESTS = 100;
+var DECLARATIONS_QUANTITY = 8;
 
 
+// функция для расчета случайного индекса элемента массива
+var calculateRandomIndex = function (arr) {
+  return Math.round(Math.random() * (arr.length - 1));
+};
 
-// Создайте массив, состоящий из 8 сгенерированных JS объектов, которые будут описывать похожие объявления неподалёку.
-// Структура объектов должна быть следующей:
-//
-// {
-//   "author": {
-//   "avatar": строка, адрес изображения вида img/avatars/user{{xx}}.png, где {{xx}} это число от 1 до 8 с ведущим нулём.
-// Например, 01, 02 и т. д. Адреса изображений не повторяются
-// },
-//
-//   "offer": {
-//   "title": строка, заголовок предложения, одно из фиксированных значений
-// "Большая уютная квартира", // "Маленькая неуютная квартира", "Огромный прекрасный дворец", "Маленький ужасный дворец",
-// "Красивый гостевой домик", "Некрасивый негостеприимный домик", "Уютное бунгало далеко от моря",
-// "Неуютное бунгало по колено в воде".
-// Значения не должны повторяться.
+// функция генерации случайного числа в интервале от min до max
+var getRandomNumber = function (min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+};
 
-//   "address": строка, адрес предложения, представляет собой запись вида
-// "{{location.x}}, {{location.y}}", например, "600, 350"
+// генерация массива с адресами аватарок
+var avatars = [];
+for (var i = 0; i < MAX_PICTURE_NUMBER; i++) {
+  avatars[i] = 'img/avatars/user0' + (i + 1) + '.png';
+}
 
-//   "price": число, случайная цена от 1000 до 1 000 000
+// функция генерации объявления по заданным параметрам
+var createRandomDeclaration = function (titles, minX, maxX, minY, maxY, minPrice, maxPrice, type, rooms, minGuests, maxGuests, checkTime, featuresOptions, photosCollection) {
+  var author = {};
+  var randomIndexAvatars = calculateRandomIndex(avatars);
+  author.avatar = avatars[randomIndexAvatars];
+  avatars.splice(randomIndexAvatars, 1);
 
-//   "type": строка с одним из четырёх фиксированных значений: palace, flat, house или bungalo
+  // выбираем случайное название без возможности повторения
+  var offer = {};
+  var randomIndexTitle = calculateRandomIndex(titles);
+  offer.titles = titles[randomIndexTitle];
+  titles.splice(randomIndexTitle, 1);
 
-//   "rooms": число, случайное количество комнат от 1 до 5
+  // вычисляем случайные координаты объекта
+  var locationX = getRandomNumber(minX, maxX);
+  var locationY = getRandomNumber(minY, maxY);
+  offer.address = locationX + ', ' + locationY;
+  // случайная цена из диапазона
+  offer.price = getRandomNumber(minPrice, maxPrice);
+  // выбираем тип жилья из доступных
+  offer.type = type[calculateRandomIndex(type)];
+  // случайное количество комнат из диапазона
+  offer.rooms = rooms[calculateRandomIndex(rooms)];
+  // случайное количество гостей из диапазона
+  offer.guests = getRandomNumber(minGuests, maxGuests);
+  // случайное время заезда/выезда из доступных
+  offer.checkin = checkTime[calculateRandomIndex(checkTime)];
+  offer.checkout = checkTime[calculateRandomIndex(checkTime)];
 
-//   "guests": число, случайное количество гостей, которое можно разместить
+  // создание массива "дополнительных услуг" случайной длины на основе массива с доступными услугами
+  var features = [];
+  features.length = getRandomNumber(MIN_ARRAY_LENGTH, featuresOptions.length);
+  for (i = 0; i < features.length; i++) {
+    var randomIndexFeatures = calculateRandomIndex(featuresOptions);
+    features[i] = featuresOptions[randomIndexFeatures];
+    featuresOptions.splice(randomIndexFeatures, 1);
+  }
+  offer.features = features;
 
-//   "checkin": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00,
+  offer.description = '';
+  // вывод доступных фотографий объекта в случайном порядке
+  var photos = [];
+  var photosCopy = photosCollection.slice();
+  for (i = 0; i < photosCollection.length; i++) {
+    var randomIndexPhotos = calculateRandomIndex(photosCopy);
+    photos[i] = photosCopy[randomIndexPhotos];
+    photosCopy.splice(randomIndexPhotos, 1);
+  }
+  offer.photos = photos;
 
-//     "checkout": строка с одним из трёх фиксированных значений: 12:00, 13:00 или 14:00
+  var location = {};
+  location.x = locationX;
+  location.y = locationY;
 
-//   "features": массив строк случайной длины из ниже предложенных: "wifi", "dishwasher", "parking", "washer",
-// "elevator", "conditioner",
+  var declaration = {};
+  declaration.author = author;
+  declaration.offer = offer;
+  declaration.location = location;
 
-//     "description": пустая строка,
+  return declaration;
+};
 
-//     "photos": массив из строк "http://o0.github.io/assets/images/tokyo/hotel1.jpg",
-// "http://o0.github.io/assets/images/tokyo/hotel2.jpg" и "http://o0.github.io/assets/images/tokyo/hotel3.jpg"
-// расположенных в произвольном порядке
-// },
-//
-//   "location": {
-//   "x": случайное число, координата x метки на карте от 300 до 900,
-//     "y": случайное число, координата y метки на карте от 150 до 500
-// }
-// }
+// создание массива сгенерированных объявлений
+var declarationsList = [];
+var titles = TITLES.slice();
+var featuresOptions = FEATURES_OPTIONS.slice();
+var photosCollection = PHOTOS_COLLECTION.slice();
+for (i = 0; i < DECLARATIONS_QUANTITY; i++) {
+  declarationsList[i] = createRandomDeclaration(titles, MIN_X, MAX_X, MIN_Y, MAX_Y, MIN_PRICE, MAX_PRICE, TYPES, ROOMS, MIN_GUESTS, MAX_GUESTS, CHECK_TIMES, featuresOptions, photosCollection);
+}
+
+var map = document.querySelector('.map');
+map.classList.remove('.map--faded');
